682c6e55fe72547c0e6bb969fc169fe52c12464a
GarikVologodskiy <49877717+GarikVologodskiy@users.noreply.github.com>
2020-09-29 22:30:13 +0500

    Lesson_dif

--- src/Lesson_1/Fuel.java
+++ src/Lesson_1/Fuel.java
@@ -4,13 +4,6 @@ import java.util.Scanner;
 
 public class Fuel {
     public static void main(String[] args){
-
-        double price = 47.5;
-        Scanner in = new Scanner(System.in);
-        System.out.println("Please enter the number of liters: ");
-        int lit = in.nextInt();
-        double cost = price * lit;
-        System.out.println("Your fuel cost is: " + cost + " RUB");
-        in.close();
+        
     }
 }
