885bbf5905a85c11ca9cc06c0726c17ba259b0fb
GarikVologodskiy <49877717+GarikVologodskiy@users.noreply.github.com>
2020-10-15 23:13:08 +0500

    Random and sort

--- src/Lesson_2/Random.java
+++ src/Lesson_2/Random.java
@@ -6,33 +6,34 @@ public class Random {
     public static void main(String[] args) {
         System.out.println("For exit enter the \"0\"" + "\n");
         int random_num = (int) (Math.random()*100-1) + 1;
-        System.out.println("Random number is: " + random_num + "");
+        /* print for checking */
         Scanner in = new Scanner(System.in);
-        int manual_num = 0;
-        int dif = 0;
+        System.out.println("Random number is: " + random_num + "");
+        int manual_num = 0, prev = 0, cur = 0, next = 0, dif =0;
 
-        while(random_num != manual_num) {
+        while (random_num != manual_num | manual_num !=0) {
             System.out.println("Please enter the number: ");
             manual_num = in.nextInt();
             System.out.println("Your number is: " + manual_num);
 
-            if (manual_num != 0) {
-               dif =  Math.abs(random_num - manual_num);
+            cur = Math.abs(random_num - prev); /* 50 */
+            next = Math.abs(cur - manual_num); /* 10 */
+
+            if (cur < next) {
+                System.out.println("Hot!");
+                }
+            else if (cur > next) {
+                System.out.println("Cold!");
+                }
+            if (random_num == manual_num) {
+                System.out.println("Matches! Our congrats!");
+                break;
             }
             else if (manual_num == 0) {
                 System.out.println("Bye!");
                 break;
             }
-            if (dif > 10) {
-                System.out.println("It's cold");
-            }
-            else if (dif > 0) {
-                System.out.println("It's hot");
-            }
-            else if (random_num == manual_num) {
-                System.out.println("Matches! Our congrats!");
-                break;
-            }
+            prev = manual_num;
         }
         in.close();
     }
--- src/Lesson_2/Sort.java
+++ src/Lesson_2/Sort.java
@@ -1,5 +1,49 @@
 package Lesson_2;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Sort {
-}
+    public static void main(String[] args) {
+        int temp;
+        int arr[] = {16, 4, 5, 1, 34, 43, 11, 9, 0, 500, 501};
+        System.out.println("Original array: ");
+        initprint(arr);
+        bubblesort(arr);
+        initprint(arr);
+        autosort(arr);
+        initprint(arr);
+    }
 
+    // Output original array
+    public static void initprint(int[] init) {
+        for (int i = 0; i < init.length; i++) {
+            System.out.print(init[i] + "; ");
+        }
+        System.out.println();
+        System.out.println("----------------");
+    }
+
+    // Output bubble sorted array
+    public static void bubblesort(int[] bsort) {
+        System.out.println("Bubble sorted array: ");
+        for (int j = 0; j < bsort.length; j++) {
+            for (int n = 0; n < bsort.length; n++) {
+                if (bsort[j] >= bsort[n]) {
+                    int temp = bsort[j];
+                    bsort[j] = bsort[n];
+                    bsort[n] = temp;
+                }
+                /*System.out.print(bsort[n] + "; ");*/
+            }
+        }
+    }
+
+    // Output auto sorted array
+    public static void autosort(int[] asort) {
+        System.out.println("Auto sorted array: ");
+        Arrays.sort(new int[][]{asort}, Collections.reverseOrder());
+        for (int i = 0; i < asort.length; i++) {
+            /*System.out.print(asort[i] + "; ");*/
+        }
+    }
+}
